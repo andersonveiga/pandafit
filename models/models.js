e7387e6e5c05b80b4b919fa6220e8c309156a3ef
Philipp Dowling <phdowling@users.noreply.github.com>
2016-09-17 17:38:51 +0200

    models yo

--- models/models.js
+++ models/models.js
@@ -72,7 +72,7 @@ var to = setTimeout(deductPoints, 10 * 1000, process.pid, process.arch);
 
 function deductPoints(id, arch){
     console.log('The process id is %d and the processor architecture is %s', id, arch);
-    User.update({"score": {"$gt": 1}}, {'$inc': {"score": -1}}).exec();
+    User.update({"score": {"$gt": 1}}, {'$inc': {"score": -1}}, {multi: true}).exec();
     to = setTimeout(deductPoints, 10 * 1000, id, arch);
 }
 console.log('done');
