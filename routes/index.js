567d2311c46be678ed20856251948de5bff87132
Philipp Dowling <phdowling@users.noreply.github.com>
2016-09-17 11:45:39 +0200

    test score json

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/score', function (req, res, next) {
+  res.send({"score": 50});  // TODO get from data
+});
+
 module.exports = router;
